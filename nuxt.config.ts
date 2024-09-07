0aba76871005caa024b47509fd7838a28f549bb2
spiderpig60 <spiderpig60@gmail.com>
2024-09-07 12:52:32 +0300

    electron initial commit with fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: ["nuxt-electron"],
+  ssr: false,
+  electron: {
+    disableDefaultOptions: true,
+    build: [
+      {
+        entry: "electron/main.ts",
+      },
+    ],
+  },
+});
