bce278524234cc196aacc0f20dad4227a0c7ff20
spiderpig60 <spiderpig60@gmail.com>
2024-09-08 00:16:04 +0300

    main.ts leaning

--- electron/main.ts
+++ electron/main.ts
@@ -1,50 +1,15 @@
-import { app, BrowserWindow, ipcMain } from "electron";
+import { app, BrowserWindow } from "electron";
 import path from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
-const __dirname = path.dirname(__filename); // get the name of the directory
-
-process.env.APP_ROOT = path.join(__dirname, "..");
-
-export const MAIN_DIST = path.join(process.env.APP_ROOT, "dist-electron");
-export const RENDERER_DIST = path.join(process.env.APP_ROOT, ".output/public");
-
-process.env.VITE_PUBLIC = process.env.VITE_DEV_SERVER_URL
-  ? path.join(process.env.APP_ROOT, "public")
-  : RENDERER_DIST;
-
-let win: BrowserWindow | null;
-
-function createWindow() {
-  win = new BrowserWindow();
+app.whenReady().then(() => {
+  const win = new BrowserWindow();
 
   if (process.env.VITE_DEV_SERVER_URL) {
     win.loadURL(process.env.VITE_DEV_SERVER_URL);
     win.webContents.openDevTools();
   } else {
-    win.loadFile(path.join(process.env.VITE_PUBLIC!, "index.html"));
+    win.loadFile(
+      path.join(process.env.APP_ROOT!, ".output/public", "index.html")
+    );
   }
-}
-
-function initIpc() {
-  ipcMain.handle("app-start-time", () => new Date().toLocaleString());
-}
-
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-    win = null;
-  }
-});
-
-app.on("activate", () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
-
-app.whenReady().then(() => {
-  initIpc();
-  createWindow();
 });
